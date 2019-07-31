f508494356cb709573c0f13402c69e9f4855317d
SudoNhim <brenton.a.milne@gmail.com>
2019-07-31 00:19:15 -0700

    Keep line/paragraph breaks in doc view

--- src/client/components/DocViewer.tsx
+++ src/client/components/DocViewer.tsx
@@ -1,43 +1,50 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { getDoc } from '../api';
-import { setDoc } from '../actions';
-import { IDoc, IDocMeta } from '../../shared/IApiTypes';
-import { IAppState } from '../model';
+import * as React from "react";
+import { connect } from "react-redux";
+import { getDoc } from "../api";
+import { setDoc } from "../actions";
+import { IDoc, IDocMeta } from "../../shared/IApiTypes";
+import { IAppState } from "../model";
 
-const css = require('./all.css');
+const css = require("./all.css");
 
 interface IProps {
-    id: string;
-    doc: IDoc;
-    docMeta: IDocMeta;
-    setDoc: typeof setDoc;
+  id: string;
+  doc: IDoc;
+  docMeta: IDocMeta;
+  setDoc: typeof setDoc;
 }
 
 class DocViewer extends React.Component<IProps> {
-    public async componentDidMount() {
-        const doc = await getDoc(this.props.id);
-        this.props.setDoc(this.props.id, doc);
-    }
+  public async componentDidMount() {
+    const doc = await getDoc(this.props.id);
+    this.props.setDoc(this.props.id, doc);
+  }
 
-    public render() {
-        if (!this.props.docMeta)
-            return <div className={css.docviewer}>Document does not exist.</div>;
-        else if (!this.props.doc)
-            return <div className={css.docviewer}>Loading...</div>;
-        else {
-            return <div className={css.docviewer}>
-                <p className={css.docviewer_title}>{this.props.docMeta.title}</p>
-                {this.props.doc.text}
-            </div>
-        }
+  public render() {
+    if (!this.props.docMeta)
+      return <div className={css.docviewer}>Document does not exist.</div>;
+    else if (!this.props.doc)
+      return <div className={css.docviewer}>Loading...</div>;
+    else {
+      return (
+        <div className={css.docviewer}>
+          <p className={css.docviewer_title}>{this.props.docMeta.title}</p>
+          {this.props.doc.text.split("\n\n").map(p => (
+            <p>{p.split("\n").map(l => <span>{l}<br /></span>)}</p>
+          ))}
+        </div>
+      );
     }
+  }
 }
 
 const mapStateToProps = (state: IAppState, ownProps) => ({
-    id: ownProps.id,
-    doc: state.docs.cache[ownProps.id],
-    docMeta: state.docs.graph[ownProps.id]
+  id: ownProps.id,
+  doc: state.docs.cache[ownProps.id],
+  docMeta: state.docs.graph[ownProps.id]
 });
 
-export default connect(mapStateToProps, { setDoc })(DocViewer);
+export default connect(
+  mapStateToProps,
+  { setDoc }
+)(DocViewer);
