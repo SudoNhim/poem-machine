50391cb8fda4d50e8b793162f08e35bbab2cd71a
Brenton Milne <brenton.a.milne@gmail.com>
2019-11-23 12:00:35 -0800

    Headers and references for content parts

--- src/client/components/docviewer/ContentPartView.tsx
+++ src/client/components/docviewer/ContentPartView.tsx
@@ -1,24 +1,42 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { IAppState } from "../../model";
-import { Metadata, Text, Prologue, Variation, Note } from "cohen-db/schema";
+import { Prologue, Variation, Note } from "cohen-db/schema";
+import { Link } from "react-router-dom";
 import CanonTextView from "./CanonTextView";
+import { IDocMeta } from "../../../shared/IApiTypes";
 
 const css = require("./docviewer.css");
 
 interface IProps {
-  part: (Prologue | Variation | Note)
+  part: Prologue | Variation | Note;
+  target: IDocMeta;
 }
 
 class ContentPartView extends React.Component<IProps> {
   public render() {
-    return <CanonTextView text={this.props.part.content} />;
+    const prefix = {
+      prologue: "Prologue to",
+      note: "On",
+      variation: "Variation of"
+    }[this.props.part.kind];
+
+    return (
+      <div>
+        {prefix}{" "}
+        <Link to={`/doc/${this.props.part.reference}`}>
+          {this.props.target.title}
+        </Link>
+        <CanonTextView text={this.props.part.content} />
+      </div>
+    );
   }
 }
 
 // currently not using redux connection
 const mapStateToProps = (state: IAppState, ownProps: IProps) => ({
-    part: ownProps.part
+  part: ownProps.part,
+  target: state.docs.graph[ownProps.part.reference]
 });
 
 export default connect(mapStateToProps)(ContentPartView);
