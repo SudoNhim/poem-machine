e0164040a6ae0789fff3f77bafd57e57e299bc11
Brenton Milne <brenton.a.milne@gmail.com>
2019-11-28 00:02:24 -0800

    Fix bugs with search tree

--- src/client/components/SearchTree.tsx
+++ src/client/components/SearchTree.tsx
@@ -1,78 +1,82 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { ISearchResults } from '../../shared/IApiTypes';
-import { IAppState } from '../model';
-import { Link } from 'react-router-dom';
-import NavTreeNode from './NavTreeNode';
-import ExpandButton from './ExpandButton';
-import { setFocus } from '../actions';
+import * as React from "react";
+import { connect } from "react-redux";
+import { ISearchResults } from "../../shared/IApiTypes";
+import { IAppState } from "../model";
+import { Link } from "react-router-dom";
+import NavTreeNode from "./NavTreeNode";
+import ExpandButton from "./ExpandButton";
+import { setFocus } from "../actions";
 
-const css = require('./all.css');
+const css = require("./all.css");
 
 interface IProps {
-    search: ISearchResults;
-    hasFocus: boolean;
-    setFocus: typeof setFocus;
+  search: ISearchResults;
+  hasFocus: boolean;
+  setFocus: typeof setFocus;
 }
 
 interface IState {
-    expanded: boolean;
+  expanded: boolean;
 }
 
 class SearchTree extends React.Component<IProps, IState> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            expanded: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false
+    };
+  }
 
-    private toggleExpand() {
-        this.setState({
-            expanded: !this.state.expanded
-        });
-    }
+  private toggleExpand() {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  }
 
-    public render() {
-        const children = this.state.expanded ?
-            this.props.search.hits.map(hit => hit.id)
-            : [];
+  public render() {
+    var ids = this.props.search.previews ?
+        this.props.search.previews.map(p => p.docRef.docId)
+        : [];
+    ids = ids.filter((id, i) => (ids.find((id2, i2) => id === id2 && i2 < i) === undefined));
 
-        const childEls = children.map((id, index) => <NavTreeNode id={id} key={index} />);
+    const children = this.state.expanded ? ids : [];
 
-        const doclinkEl =
-            <Link to={`/search/${this.props.search.term}`}>
-                <span className={css.navtreenode_doclink} >
-                    {this.props.search.term ?
-                        "Search: " + this.props.search.term
-                        : "No search..."}
-                </span>
-            </Link>;
+    const childEls = children.map((id, index) => (
+      <NavTreeNode id={id} key={index} />
+    ));
 
-        const titleClasses = [css.navtreenode_title];
-        if (this.props.hasFocus)
-            titleClasses.push(css.navtreenode_title_focus);
+    const doclinkEl = (
+      <Link to={`/search/${this.props.search.term}`}>
+        <span className={css.navtreenode_doclink}>
+          {this.props.search.term
+            ? "Search: " + this.props.search.term
+            : "No search..."}
+        </span>
+      </Link>
+    );
 
-        return (
-        <div className={css.navtreenode}>
-            <div className={titleClasses.join(" ")}>
-                <ExpandButton
-                    expanded={this.state.expanded}
-                    canExpand={this.props.search.hits.length > 0}
-                    onClick={() => this.toggleExpand()}
-                />
-                {doclinkEl}
-            </div>
-            <div className={css.navtreenode_content}>
-                {childEls}
-            </div>
-        </div>)
-    }
+    const titleClasses = [css.navtreenode_title];
+    if (this.props.hasFocus) titleClasses.push(css.navtreenode_title_focus);
+
+    return (
+      <div className={css.navtreenode}>
+        <div className={titleClasses.join(" ")}>
+          <ExpandButton
+            expanded={this.state.expanded}
+            canExpand={this.props.search.hits.length > 0}
+            onClick={() => this.toggleExpand()}
+          />
+          {doclinkEl}
+        </div>
+        <div className={css.navtreenode_content}>{childEls}</div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state: IAppState) => ({
-    search: state.search,
-    hasFocus: state.focus.search
+  search: state.search,
+  hasFocus: state.focus.search
 });
 
 export default connect(mapStateToProps, { setFocus })(SearchTree);
