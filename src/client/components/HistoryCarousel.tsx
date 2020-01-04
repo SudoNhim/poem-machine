32caa93cde2833287af5b027328ad1e3b27e822f
Brenton Milne <brenton.a.milne@gmail.com>
2020-01-04 15:17:09 -0800

    Make historycarousel remember scroll

--- src/client/components/HistoryCarousel.tsx
+++ src/client/components/HistoryCarousel.tsx
@@ -1,29 +1,34 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { IAppState, IFocusState, IFocusStateArray } from '../model';
-import SwipableViews from 'react-swipeable-views';
-import FocusContent from './FocusContent';
+import * as React from "react";
+import { connect } from "react-redux";
+import { IAppState, IFocusStateArray } from "../model";
+import SwipableViews from "react-swipeable-views";
+import FocusContent from "./FocusContent";
 
-const css = require('./all.css');
+const css = require("./all.css");
 
 interface IProps {
-    focusStates: IFocusStateArray;
+  focusStates: IFocusStateArray;
 }
 
-interface IState {
-}
+interface IState {}
 
 class HistoryCarousel extends React.Component<IProps, IState> {
-    public render() {
-        return <SwipableViews index={this.props.focusStates.index}>
-            {this.props.focusStates.frames.map((frame, i) =>
-                <FocusContent key={i} focus={frame} />)}
-        </SwipableViews>
-    }
+  public render() {
+    return (
+      <SwipableViews
+        style={{position:"absolute", top:"0em", bottom:"-0em"}}
+        index={this.props.focusStates.index}
+      >
+        {this.props.focusStates.frames.map((frame, i) => (
+          <FocusContent key={i} focus={frame} />
+        ))}
+      </SwipableViews>
+    );
+  }
 }
 
 const mapStateToProps = (state: IAppState): IProps => ({
-    focusStates: state.focus
+  focusStates: state.focus
 });
 
 export default connect(mapStateToProps, {})(HistoryCarousel);
