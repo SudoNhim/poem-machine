41518de8bbc7f7cdd8a861ecdae64a4341c288e1
Brenton Milne <brenton.a.milne@gmail.com>
2020-08-08 19:29:34 -0700

    Hover annotation works completely

--- src/client/components/docviewer/CanonTextView.tsx
+++ src/client/components/docviewer/CanonTextView.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { IAppState } from "../../model";
+import { IAppState, IFocusState, IHoverState } from "../../model";
 import { Text } from "cohen-db/schema";
 
 const css = require("./docviewer.css");
@@ -8,6 +8,7 @@ const css = require("./docviewer.css");
 interface IProps {
   text: Text;
   prefix: string;
+  hover: IHoverState;
 }
 
 class CanonTextView extends React.Component<IProps> {
@@ -21,7 +22,11 @@ class CanonTextView extends React.Component<IProps> {
 
   private renderParagraph(pi: number, text: string | string[]): JSX.Element {
     const id = `${this.props.prefix}p${pi}`;
-    return <div id={id} className={css.canonparagraph} key={pi}>
+    const classNames: string[] = [css.canonparagraph];
+    if (this.props.hover.docParts && this.props.hover.docParts.indexOf(id) >= 0)
+      classNames.push(css.canonhover);
+
+    return <div id={id} className={classNames.join(" ")} key={pi}>
       {Array.isArray(text)
       ? text.map((line, li) => this.renderLine(pi, li, line))
       : text}
@@ -30,8 +35,12 @@ class CanonTextView extends React.Component<IProps> {
 
   private renderLine(pi: number, li: number, s: string): JSX.Element {
     const id = `${this.props.prefix}p${pi}.l${li}`;
+    const classNames: string[] = [css.canonlinetext];
+    if (this.props.hover.docParts && this.props.hover.docParts.indexOf(id) >= 0)
+      classNames.push(css.canonhover);
+
     return <p className={css.canonline} key={li}>
-      <span id={id} className={css.canonlinetext}>{s}</span>
+      <span id={id} className={classNames.join(" ")}>{s}</span>
     </p>;
   }
 }
@@ -39,7 +48,8 @@ class CanonTextView extends React.Component<IProps> {
 // currently not using redux connection
 const mapStateToProps = (state: IAppState, ownProps: IProps) => ({
   text: ownProps.text,
-  prefix: ownProps.prefix
+  prefix: ownProps.prefix,
+  hover: state.hover
 });
 
 export default connect(mapStateToProps)(CanonTextView);
