a320811120072ee643a9164c286b0b8fee99bf52
Brenton Milne <brenton.a.milne@gmail.com>
2020-08-16 21:14:33 -0700

    display annotations

--- src/client/components/annotator/Annotator.tsx
+++ src/client/components/annotator/Annotator.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { setDoc, setScrolled } from "../../actions";
-import { IAppState, IFocusState } from "../../model";
+import { IAppState, IFocusState, IDocState } from "../../model";
+import { IAnnotation } from "../../../shared/IApiTypes";
+import { SerializeDocRef } from "../../../shared/util";
 
 const css = require("./annotator.css");
 
 interface IProps {
   focus: IFocusState;
+  docs: IDocState;
 }
 
 interface IState {
@@ -19,19 +22,33 @@ class Annotator extends React.Component<IProps, IState> {
 
   public render() {
     const docRef = this.props.focus.docRef;
-    if (docRef && (docRef.line || docRef.paragraph)) {
+    if (!docRef || (!docRef.line && !docRef.paragraph))
+      return null;
+    
+    const doc = this.props.docs.cache[docRef.docId];
+    if (!doc)
+      return null;
+
+    const refPart = SerializeDocRef(docRef).split('#')[1];
+    const annotations = (doc.annotations || []).filter(annotation =>
+      !!(annotation.canonRefs.find(ref => ref === refPart))
+    );
+
     return <div className={css.annotatorcontainer}>
-        This panel will let you view, create, edit annotations on a selection
-        {JSON.stringify(this.props.focus)}
+        {annotations.map((anno, i) => this.renderAnnotation(anno, i))}
     </div>;
-    } else {
-        return null;
-    }
+  }
+
+  private renderAnnotation(annotation: IAnnotation, key: number): JSX.Element {
+    return <div className={css.annotation} key={key}>
+      {annotation.text}
+    </div>;
   }
 }
 
 const mapStateToProps = (state: IAppState, ownProps) => ({
-  focus: state.focus
+  focus: state.focus,
+  docs: state.docs
 });
 
 export default connect(mapStateToProps, { setDoc, setScrolled })(Annotator);
