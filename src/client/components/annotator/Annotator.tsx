a39457fea5bed0d3d5bc0847a43facc5c99f96dc
Brenton Milne <brenton.a.milne@gmail.com>
2020-08-16 21:40:58 -0700

    more annotation UI

--- src/client/components/annotator/Annotator.tsx
+++ src/client/components/annotator/Annotator.tsx
@@ -33,10 +33,48 @@ class Annotator extends React.Component<IProps, IState> {
     const annotations = (doc.annotations || []).filter(annotation =>
       !!(annotation.canonRefs.find(ref => ref === refPart))
     );
+    const onContainingParagraph = (doc.annotations || []).filter(annotation =>
+      (refPart.split('.l').length > 1)
+      && (!!annotation.canonRefs.find(ref => ref === refPart.split('.l')[0]))
+    );
+    const childOfFocused = (doc.annotations || []).filter(annotation =>
+      (!!annotation.canonRefs.find(ref => ref.startsWith(refPart)))
+    );
 
     return <div className={css.annotatorcontainer}>
+      <p>
+        {this.renderFocusString()}
+        {this.renderCreateNew()}
         {annotations.map((anno, i) => this.renderAnnotation(anno, i))}
+      </p>
+      {onContainingParagraph.length > 0 && <p>
+        Related
+        {onContainingParagraph.map((anno, i) => this.renderAnnotation(anno, i))}
+      </p>}
+      { childOfFocused.length > 0 && <p>
+        Related
+        {childOfFocused.map((anno, i) => this.renderAnnotation(anno, i))}
+      </p>}
     </div>;
+  }
+
+  private renderFocusString(): string {
+    var terms: string[] = [];
+    const docRef = this.props.focus.docRef;
+    if (docRef.section)
+      terms.push(`section ${docRef.section}`);
+    if (docRef.paragraph)
+      terms.push(`paragraph ${docRef.paragraph}`);
+    if (docRef.line)
+      terms.push(`line ${docRef.line}`);
+
+    return `On ${terms.join(', ')}`;
+  }
+
+  private renderCreateNew(): JSX.Element {
+    return <div className={css.newannotation}>
+    Create a new annotation
+  </div>;
   }
 
   private renderAnnotation(annotation: IAnnotation, key: number): JSX.Element {
