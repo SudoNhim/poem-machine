184bdc66b33eb56c48d7305193a24967b95fb0a7
Brenton Milne <brmilne@microsoft.com>
2020-10-18 00:14:28 -0700

    see if this fixes crash in heroku

--- src/server/main.ts
+++ src/server/main.ts
@@ -12,39 +12,43 @@ import { apiRouter } from "./routes/api-router";
 import { pagesRouter } from "./routes/pages-router";
 import { staticsRouter } from "./routes/statics-router";
 
-const app = express();
-app.set("view engine", "ejs");
+try {
+  const app = express();
+  app.set("view engine", "ejs");
 
-// Auth
-app.use(
-  expressSession({
-    secret: COOKIE_SECRET || "testsecret",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 10000000000, // ~3mo max cookie age
-    },
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+  // Auth
+  app.use(
+    expressSession({
+      secret: COOKIE_SECRET || "testsecret",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 10000000000, // ~3mo max cookie age
+      },
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-// Auth config
-passport.use(Account.createStrategy());
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
+  // Auth config
+  passport.use(Account.createStrategy());
+  passport.serializeUser(Account.serializeUser());
+  passport.deserializeUser(Account.deserializeUser());
 
-if (MONGODB_STR) mongoose.connect(MONGODB_STR);
-else createTestDatabase("test");
+  if (MONGODB_STR) mongoose.connect(MONGODB_STR);
+  else createTestDatabase("test");
 
-// Routing
-app.use("/assets", express.static(path.join(process.cwd(), "assets")));
-app.use("/api", apiRouter());
-app.use("/statics", staticsRouter());
+  // Routing
+  app.use("/assets", express.static(path.join(process.cwd(), "assets")));
+  app.use("/api", apiRouter());
+  app.use("/statics", staticsRouter());
 
-// Everything not matched by the above falls through to the app page
-app.use(pagesRouter());
+  // Everything not matched by the above falls through to the app page
+  app.use(pagesRouter());
 
-app.listen(SERVER_PORT, () => {
-  console.log(`App listening on port ${SERVER_PORT}!`);
-});
+  app.listen(SERVER_PORT, () => {
+    console.log(`App listening on port ${SERVER_PORT}!`);
+  });
+} catch (err) {
+  console.log(err);
+}
