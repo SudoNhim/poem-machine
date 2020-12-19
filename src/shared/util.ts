23cd14c9ec2d8ce2e0608191b1d30a2a721bb43e
Brenton Milne <brenton.a.milne@gmail.com>
2020-12-19 00:02:34 -0600

    re-implement reference serialization

--- src/shared/util.ts
+++ src/shared/util.ts
@@ -1,51 +1,24 @@
-import { isNullOrUndefined } from "util";
+import { Reference } from "cohen-db/schema";
 
-import { IDocReference } from "./ApiTypes";
-
-export function SerializeDocRef(
-  ref: IDocReference,
-  includeSubstr: boolean = true
-): string {
-  const parts: string[] = [];
-  if (!isNullOrUndefined(ref.section)) parts.push(`s${ref.section}`);
-  if (!isNullOrUndefined(ref.paragraph)) parts.push(`p${ref.paragraph}`);
-  if (!isNullOrUndefined(ref.line)) parts.push(`l${ref.line}`);
-
-  if (!isNullOrUndefined(ref.substrings) && includeSubstr)
-    parts.push(ref.substrings.map((s) => `i${s[0]}-${s[1]}`).join(""));
-
-  return `${ref.docId}${parts.length ? "#" : ""}${parts.join(".")}`;
+export function SerializeDocRef(ref: Reference): string {
+  switch (ref.kind) {
+    case "document":
+      return `${ref.documentId}`;
+    case "section":
+      return `${ref.documentId}#${ref.sectionId}/`;
+    case "fragment":
+      return ref.sectionId
+        ? `${ref.documentId}#${ref.sectionId}/${ref.fragmentId}`
+        : `${ref.documentId}#${ref.fragmentId}`;
+  }
 }
 
-export function DeserializeDocRef(ref: string): IDocReference {
-  const [docId, parts] = ref.split("#");
-  const out: IDocReference = {
-    docId,
-  };
-
-  if (!parts) return out;
-
-  for (var part of parts.split(".")) {
-    const code = part[0];
-    const valueStr = part.substring(1);
-    switch (code) {
-      case "s":
-        out.section = Number.parseInt(valueStr);
-        break;
-      case "p":
-        out.paragraph = Number.parseInt(valueStr);
-        break;
-      case "l":
-        out.line = Number.parseInt(valueStr);
-        break;
-      case "i":
-        out.substrings = valueStr
-          .split("i")
-          .map((s) => s.split("-").map(Number.parseInt));
-      default:
-        throw "Unrecognized code in docref";
-    }
-  }
-
-  return out;
+export function DeserializeDocRef(ref: string): Reference {
+  const [documentId, rest] = ref.split("#");
+  if (!rest) return { kind: "document", documentId };
+  if (!rest.includes("/"))
+    return { kind: "fragment", documentId, fragmentId: rest };
+  const [sectionId, fragmentId] = rest.split("/");
+  if (!fragmentId) return { kind: "section", documentId, sectionId };
+  else return { kind: "fragment", documentId, sectionId, fragmentId };
 }
